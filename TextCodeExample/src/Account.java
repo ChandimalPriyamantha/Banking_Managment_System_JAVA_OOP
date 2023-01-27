20e7a8467522e5953332c59bc8a952d506efb751
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2023-01-28 00:03:55 +0530

    Adding new features

--- TextCodeExample/src/Account.java
+++ TextCodeExample/src/Account.java
@@ -1,12 +1,23 @@
 //Account class with a constructor that initializes the name
 public class Account {
     private String name; //instance variable
-
+    private double balance; // instance varibale
 
     //constructor initializes name with parameter name
-    public Account(String name){
+    public Account(String name, double balance){
         this.name = name;
 
+        if (balance>0.0)
+            this.balance = balance;
+    }
+
+    public void deposit(double depositAmount){
+        if(depositAmount>0.0)
+            balance = balance + depositAmount;
+    }
+
+    public double getBalance(){
+        return balance;
     }
 
     // method to set the name
--- TextCodeExample/src/AccountText.java
+++ TextCodeExample/src/AccountText.java
@@ -1,11 +1,11 @@
 public class AccountText{
 
     public static void main(String[] args){
-        Account account1 = new Account("Chandimal");
-        Account account2 = new Account("Tharuka");
+        Account account1 = new Account("Chandimal",100.0);
+        Account account2 = new Account("Tharuka",200.0);
 
-        System.out.printf("account name is:%s%n",account1.getName());
-        System.out.printf("account name is:%s%n",account2.getName());
+        System.out.printf("account name is : %s%n",account1.getName());
+        System.out.printf("account balance is : %s%n",account1.getBalance()+"$");
 
     }
 
