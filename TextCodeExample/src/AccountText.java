15f43d4626b3fefdd6834b520348dd880d52be7e
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2023-01-28 15:01:59 +0530

    Adding new features

--- TextCodeExample/src/AccountText.java
+++ TextCodeExample/src/AccountText.java
@@ -1,12 +1,42 @@
+import java.util.Scanner;
+
 public class AccountText{
 
     public static void main(String[] args){
         Account account1 = new Account("Chandimal",100.0);
-        Account account2 = new Account("Tharuka",200.0);
+        Account account2 = new Account("Tharuka",-7.0);
 
-        System.out.printf("account name is : %s%n",account1.getName());
-        System.out.printf("account balance is : %s%n",account1.getBalance()+"$");
+        //display initial balance of each object.
+        System.out.printf("%s balance: $%.2f%n",account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n%n",account2.getName(),account2.getBalance());
 
-    }
+        //create a Scanner to obtain input from the command window
+        Scanner input = new Scanner(System.in);
 
-}
+        System.out.print("Enter deposit amount for account1: ");
+        double depositAmount = input.nextDouble(); // obtain user input
+        System.out.printf("%nadding %.2f to account1 balance%n%n", depositAmount);
+        account1.deposit(depositAmount); //add to account1's balance
+
+        //display balance
+        System.out.printf("%s balance: $%.2f%n",
+                account1.getName(), account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n%n",
+                account2.getName(),account2.getBalance());
+
+        System.out.print("Enter deposit amount for account2: ");
+        depositAmount = input.nextDouble();
+        System.out.printf("%nadding %.2f%n to account2 balance%n%n",
+                depositAmount);
+        account2.deposit(depositAmount);
+
+        //display balance
+        System.out.printf("%s balance: $%.2f%n",
+                account1.getName(),account1.getBalance());
+        System.out.printf("%s balance: $%.2f%n%n",
+                account2.getName(),account2.getBalance());
+
+
+    }// end main
+
+}// end class Account
